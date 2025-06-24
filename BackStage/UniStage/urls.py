dc2b505d3d9d6491118dac57f646cf459728ec67
PRESSON <142119095+arthurvictor99-0@users.noreply.github.com>
2025-06-24 09:56:27 -0300

    Verificando para dherick

--- BackStage/UniStage/urls.py
+++ BackStage/UniStage/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# UniStageBackEND/BackStage/UniStage/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Apenas UMA linha para toda a API. Limpo e escalável.
+    # Qualquer URL que começar com /api/ será procurada no arquivo accounts.urls
     path('accounts/', include('accounts.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- BackStage/accounts/urls.py
+++ BackStage/accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('hello', views.index, name ='index'),
-    path('testCreateCompany', views.cadastrationCompany, name = 'cadastrationCompany'),
-    #path('listCompany', views.listUserCompany_view, name = 'listCompany'),
-    path('listUser', views.listUser, name = 'listUser'),
-    path('listCompany', views.listCompany, name = 'listCompany'),
-    path('listTelephone', views.listTelephone, name = 'listTelephone'),
-    path('listAdress', views.listAdress, name = 'listAdress'),
+    path('hello/', views.index, name='index'),
+    path('testCreateCompany/', views.cadastrationCompany, name='cadastrationCompany'),
+    path('listUser/', views.listUser, name='listUser'),
+    path('listCompany/', views.listCompany, name='listCompany'),
+    path('listTelephone/', views.listTelephone, name='listTelephone'),
+    path('listAdress/', views.listAdress, name='listAdress'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
